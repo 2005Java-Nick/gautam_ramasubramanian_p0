e20d9592cbcbc3c8a653b5b857151187c0300890
Gautam <gramasu000@citymail.cuny.edu>
2020-05-07 13:39:04 -0400

    Basic Maven Project Setup

---
+++ src/main/java/com/revature/projectzero/App.java
@@ -0,0 +1,13 @@
+package com.revature.projectzero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
