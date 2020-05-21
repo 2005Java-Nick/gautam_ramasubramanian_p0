def5d2cd601ed065984cb47c5c28a4ac672c576e
Gautam <gramasu000@citymail.cuny.edu>
2020-05-21 08:06:07 -0400

    Added tree diagram to clone and pull

--- src/main/java/com/revature/projectzero/App.java
+++ src/main/java/com/revature/projectzero/App.java
@@ -69,6 +69,7 @@ public class App {
       for (int i = 1; i < branchNames.size(); i++) {
         history.addBranch(branchNames.get(i));
       }
+      consoleDriver.getTreeDiagram(history); 
       serializer.createDirectory(saveImageDir);
       serializer.writeToFile(saveImagePath, history);
     }
@@ -85,6 +86,7 @@ public class App {
       history = serializer.readFromFile(saveImagePath);
       String branchName = consoleDriver.branchNameInput();
       dao.pull(branchName, history);
+      consoleDriver.getTreeDiagram(history); 
       serializer.writeToFile(saveImagePath, history);   
     }
 
